c7835cad73109c48e7c64ff1e5b7d66cd5877104
IPIvliev <cccp1945@mail.ru>
2015-01-12 18:40:51 +0300

    Add device

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+  root 'static_pages#index'
+  
   resources :articles
 
-  root 'static_pages#index'
+# Users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  resources :users
+  get "omniauth_callbacks/facebook"
+  get "omniauth_callbacks/vkontakte"
 
   get 'static_pages/index'
-
   get 'static_pages/about'
-
   get 'static_pages/contacts'
 
 end
